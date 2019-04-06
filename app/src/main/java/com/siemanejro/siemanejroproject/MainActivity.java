298a8ab670f4b89e8a0a5ab184943346d367294c
Mikołaj Stoch <mikolajstoch@gmail.com>
2019-04-06 10:33:20 +0200

    walaszek

--- app/src/main/java/com/siemanejro/siemanejroproject/MainActivity.java
+++ app/src/main/java/com/siemanejro/siemanejroproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.siemanejro.siemanejroproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Intent intent = new Intent(MainActivity.this,Tipp.class);
+        //startActivity(intent);
+
+
+
     }
 }
