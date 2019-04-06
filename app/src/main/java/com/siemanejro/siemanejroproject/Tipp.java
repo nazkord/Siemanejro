ab2c15f5b653c3437b6d8af43a972ff703ed3217
Mikołaj Stoch <mikolajstoch@gmail.com>
2019-04-06 10:31:17 +0200

    walaszek

---
+++ app/src/main/java/com/siemanejro/siemanejroproject/Tipp.java
@@ -0,0 +1,13 @@
+package com.siemanejro.siemanejroproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tipp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipp);
+    }
+}
