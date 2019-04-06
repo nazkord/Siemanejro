bb2b935c0d90dc4d784c5c4fea262efa8280478e
nazkord <nazkord@gmail.com>
2019-04-06 17:50:24 +0200

    Add some little changes

--- app/src/main/java/model/AllMatches.java
+++ app/src/main/java/model/AllMatches.java
@@ -1,21 +1,34 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import json.UtcConverter;
 import model.Match;
 
 public class AllMatches {
-    ArrayList<Match> matches;
+    private ArrayList<Match> matches;
 
     public  ArrayList<Match> getMatches() {
         return matches;
+    }
+
+    // map contains matches id and scores
+    private Map<String, Score> usersMatches = new HashMap<>();
+
+    public Map<String, Score> getUsersMatches() {
+        return usersMatches;
     }
 
     public void update (){
         for (Match match : matches){
             match.setMatchDate(UtcConverter.utcConverter(match.getUtcDate()));
         }
+    }
+
+    public void updateUserMatches() {
+
     }
 
 
