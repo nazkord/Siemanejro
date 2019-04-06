56085b309ab8e8ba89a0d4877483c4604828153f
nazkord <nazkord@gmail.com>
2019-04-06 11:28:24 +0200

    Some edits

--- app/src/main/java/com/siemanejro/siemanejroproject/AddMatchActivity.java
+++ app/src/main/java/com/siemanejro/siemanejroproject/AddMatchActivity.java
@@ -6,22 +6,57 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import model.Match;
 
 public class AddMatchActivity extends AppCompatActivity {
+
+    EditText input_date;
+    EditText input_nameFirstTeam;
+    EditText input_nameSecondTeam;
+    Button addButton;
+    ArrayList<Match> matchesArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_match);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        addToolbar();
+
+        input_date = (EditText) findViewById(R.id.input_date);
+        input_nameFirstTeam = (EditText) findViewById(R.id.input_first_team);
+        input_nameSecondTeam = (EditText) findViewById(R.id.input_second_team);
+        addButton = (Button) findViewById(R.id.addButton);
+
+        addButtonClicked();
+
+    }
+
+    private void addToolbar() {
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbarAddMatch);
+        setSupportActionBar(Toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Add Match");
+        }
+    }
+
+    private void addButtonClicked() {
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String dateMatch = input_date.getText().toString();
+                String nameFirstTeam = input_nameFirstTeam.getText().toString();
+                String nameSecondTeam = input_nameSecondTeam.getText().toString();
+                matchesArray.add(new Match(dateMatch,nameFirstTeam,nameSecondTeam));
+                input_date.setText("");
+                input_nameFirstTeam.setText("");
+                input_nameSecondTeam.setText("");
             }
         });
     }
--- app/src/main/java/com/siemanejro/siemanejroproject/MainActivity.java
+++ app/src/main/java/com/siemanejro/siemanejroproject/MainActivity.java
@@ -1,13 +1,31 @@
 package com.siemanejro.siemanejroproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addMatchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addMatchButton = (Button) findViewById(R.id.openAddMatchActivity);
+
+        addMatchButtonClicked();
+    }
+
+    private void addMatchButtonClicked() {
+        addMatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddMatchActivity.class));
+            }
+        });
     }
 }
